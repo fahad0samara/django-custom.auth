8f133e798fe3666efceb2398b83e5ccdd061e5f6
fahad <fah8ad@outlook.com>
2024-06-14 22:14:27 +0300

    add

--- auth/auth0/models.py
+++ auth/auth0/models.py
@@ -1,3 +1,18 @@
+# custom_app/models.py
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
-# Create your models here.
+class CustomUser(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    bio = models.TextField(blank=True, null=True)
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+    
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    def __str__(self):
+        return self.username
--- auth/auth0/views.py
+++ auth/auth0/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+# custom_app/views.py
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import CustomUser
+from .forms import CustomUserCreationForm, LoginForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = CustomUser.objects.filter(username=username).first()
+            if user and user.check_password(password):
+                request.session['user_id'] = user.id
+                return redirect('home')
+            else:
+                return render(request, 'login.html', {'form': form, 'error': 'Invalid username or password'})
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+def user_logout(request):
+    if 'user_id' in request.session:
+        del request.session['user_id']
+    return redirect('home')
+
+def home(request):
+    user_id = request.session.get('user_id')
+    if user_id:
+        user = CustomUser.objects.get(id=user_id)
+    else:
+        user = None
+    return render(request, 'home.html', {'user': user})
